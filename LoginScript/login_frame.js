f409dc854286c45c755d85db4729f52a1dd82136
Tanish Mudaliar <tanish.g.mudaliar@gmail.com>
2025-07-17 14:43:36 -0400

    Set env for react

---
+++ LoginScript/login_frame.js
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+
+const LoginPage = () => {
+    const [email, username] = useState(' ');
+    const [password, setPassword] = useState(' ');
+
+    const handleSubmit = (e) 
+}
